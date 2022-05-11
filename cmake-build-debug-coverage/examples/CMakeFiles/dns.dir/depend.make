aa81b8c4b47e1547013b70b5eb981f25374bd1e5
Dmitry Fedorov <df@targetai.dev>
2022-05-11 11:54:38 +0600

    5-implement-async-net added docs

--- cmake-build-debug-coverage/examples/CMakeFiles/dns.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for dns.
-# This may be replaced when dependencies are built.
--- cmake-build-debug-coverage/tests/ArrayTest_include-b858cb2.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/df/Documents/workspace/async_runtime/cmake-build-debug-coverage/tests/ArrayTest_tests-b858cb2.cmake")
-  include("/home/df/Documents/workspace/async_runtime/cmake-build-debug-coverage/tests/ArrayTest_tests-b858cb2.cmake")
-else()
-  add_test(ArrayTest_NOT_BUILT-b858cb2 ArrayTest_NOT_BUILT-b858cb2)
-endif()
