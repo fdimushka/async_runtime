aec5a79df680d4f11cd62eb7ee069b804a7f465e
Dmitry Fedorov <fedorov.dmitriy.i@gmail.com>
2024-04-09 20:40:28 +0500

    implemented tbb executor

--- cmake/tbb.cmake
+++ cmake/tbb.cmake
@@ -2,10 +2,10 @@
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__TBB_DYNAMIC_LOAD_ENABLED=0")
 set(TBB_TEST OFF)
 add_subdirectory(third_party/oneTBB)
-find_package(TBB 2021 CONFIG REQUIRED)
 
 list(APPEND SOURCES src/tbb/tbb_executor.cpp)
 list(APPEND SOURCES src/tbb/tbb_stream.cpp)
 list(APPEND SOURCES src/tbb/tbb_delayed_scheduler.cpp)
 
+include_directories(third_party/oneTBB/include)
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/src/tbb)
