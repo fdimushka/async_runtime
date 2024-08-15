5431e853c52e7cd260b71bf38d4127a110d5c4a9
Dmitry Fedorov <fedorov.dmitriy.i@gmail.com>
2024-08-15 18:27:00 +0300

    fix build for arm64

--- src/thread_executor.cpp
+++ src/thread_executor.cpp
@@ -4,27 +4,6 @@
 using namespace AsyncRuntime;
 
 
-void ThreadHelper::SetName(const char* name)
-{
-    int err = pthread_setname_np(pthread_self(), name);
-    if(err)
-        AR_LOG_SS(Error, "Error setting name for thread: " << err );
-}
-
-
-std::string ThreadHelper::GetName()
-{
-    size_t buff_size = 16;
-    char buff[buff_size];
-    int err = pthread_getname_np(pthread_self(), &buff[0], buff_size);
-    if(err)
-        AR_LOG_SS(Error, "Error getting name for thread: " << err );
-
-    std::string str(&buff[0], buff_size);
-    return std::move(str.substr (0,str.find('\0')));
-}
-
-
 ThreadExecutor::~ThreadExecutor() {
     Join();
 }
